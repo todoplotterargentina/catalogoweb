1e3f53784885eff028a03473f8a9c2d1c4690983
Tomas <ciurca.tomas@hotmail.com>
2026-04-13 23:08:51 -0300

    restauro version estable funcionando

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const publicPath = path.join(__dirname, "public");
 const pedidosPath = path.join(__dirname, "pedidos");
 
 const ADMIN_USER = process.env.ADMIN_USER || "admin";
-const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || "TodoPlotter2100!";
+const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || "cambiar-esta-clave";
 
 if (!fs.existsSync(pedidosPath)) {
   fs.mkdirSync(pedidosPath, { recursive: true });
